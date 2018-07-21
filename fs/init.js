42816fb24dc46c8953f2a8471b803e89097c20b6
Andrejus <andrejus@think-engineer.com>
2018-07-21 19:35:33 +0100

    Update sketch to post data

--- fs/init.js
+++ fs/init.js
@@ -1,49 +1,50 @@
+/* Imports */
 load('api_config.js');
 load('api_events.js');
 load('api_gpio.js');
-load('api_mqtt.js');
 load('api_net.js');
 load('api_sys.js');
 load('api_timer.js');
+load('api_http.js');
 
 let led = Cfg.get('pins.led');
 let button = Cfg.get('pins.button');
-let topic = '/devices/' + Cfg.get('device.id') + '/events';
+let device = Cfg.get('device.id');
 
-print('LED GPIO:', led, 'button GPIO:', button);
+print("Starting device ", device);
 
 let getInfo = function() {
-  return JSON.stringify({
-    total_ram: Sys.total_ram(),
-    free_ram: Sys.free_ram()
-  });
+	return JSON.stringify({
+		total_ram: Sys.total_ram(),
+		free_ram: Sys.free_ram()
+	});
 };
 
-// Blink built-in LED every second
-GPIO.set_mode(led, GPIO.MODE_OUTPUT);
-Timer.set(1000 /* 1 sec */, Timer.REPEAT, function() {
-  let value = GPIO.toggle(led);
-  print(value ? 'Tick' : 'Tock', 'uptime:', Sys.uptime(), getInfo());
-}, null);
-
-// Publish to MQTT topic on a button press. Button is wired to GPIO pin 0
+/* Send data on button press */
 GPIO.set_button_handler(button, GPIO.PULL_UP, GPIO.INT_EDGE_NEG, 20, function() {
-  let message = getInfo();
-  let ok = MQTT.pub(topic, message, 1);
-  print('Published:', ok, topic, '->', message);
+	let message = getInfo();
+	HTTP.query({
+		url: 'https://us-central1-iowine-cloud.cloudfunctions.net/pushData',
+		body: message,
+		success: function(x, y) {
+			print("Success: ", x, y);
+		},
+		error: function(error) {
+			print("Error: ", error);
+		}
+	});
+	print('Request sent.');
+	
 }, null);
 
 // Monitor network connectivity.
 Event.addGroupHandler(Net.EVENT_GRP, function(ev, evdata, arg) {
-  let evs = '???';
-  if (ev === Net.STATUS_DISCONNECTED) {
-    evs = 'DISCONNECTED';
-  } else if (ev === Net.STATUS_CONNECTING) {
-    evs = 'CONNECTING';
-  } else if (ev === Net.STATUS_CONNECTED) {
-    evs = 'CONNECTED';
-  } else if (ev === Net.STATUS_GOT_IP) {
-    evs = 'GOT_IP';
-  }
-  print('== Net event:', ev, evs);
+	print('Connection ev: ', ev)
+	print('Connection evdata: ', evdata)
+	print('Connection arg: ', arg)
+	if (ev === Net.STATUS_DISCONNECTED) {
+		connected = false;
+	} else if (ev == Net.STATUS_CONNECTED) {
+		connected = true;
+	}
 }, null);
