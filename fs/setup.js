da25ad55f1cc1da34ce744fc443c9976631d23c5
Andrejus <andrejus@think-engineer.com>
2019-03-11 00:07:47 +0000

    Clean up device config and add user support

--- fs/setup.js
+++ fs/setup.js
@@ -1,53 +1,81 @@
+
+/*
+  Entry point.
+*/
+
 /* Get current config */
-$.ajax({
-  url: '/rpc/Config.Get',
-  success: function(data) {
-    $('#ssid').val(data.wifi.sta.ssid);
-    $('#pass').val(data.wifi.sta.pass);
-  },
-});
+$.ajax({ url: '/rpc/Config.Get', success: setFields });
 
 /* On submit */
-$('#save').on('click', function() {
+$('#save').on('click', setConfig);
 
-  /* Set config */
+
+
+/**
+ * Update fields from Config.Get.
+ * @param {*} data Config values.
+ */
+let setFields = function(data) {
+  $('#ssid').val(data.wifi.sta.ssid);
+  $('#pass').val(data.wifi.sta.pass);
+  $('#user').val(data.wifi.sta.user);
+  $('#device').val(data.device.id);
+};
+
+/**
+ * Set and save input config values.
+ */
+let setConfig = function() {
+
+  /* Setup object */
+  let wifiSettings = {
+    config: {
+      wifi: {
+        ap: {
+          enable: false
+        },
+        sta: {
+          enable: true, 
+          ssid: $('#ssid').val(), 
+          pass: $('#pass').val()
+        } 
+      }
+    }
+  };
+
+  /* Get optional input */
+  let user = $('#user').val();
+  if (user != null) {
+    wifiSettings.config.wifi.sta.user = user;
+  }
+
+  /* Execute config and save */
   $.ajax({
     url: '/rpc/Config.Set',
-    data: JSON.stringify({
-      config: {
-        wifi: {
-          ap: {
-            enable: false
-          },
-          sta: {
-            enable: true, 
-            ssid: $('#ssid').val(), 
-            pass: $('#pass').val()
-          } 
-        }
-      }
-    }),
+    data: JSON.stringify(wifiSettings),
     type: 'POST',
-
-    /* On set */
-    success: function(data) {
-
-      /* Save config */
-      $.ajax({
-        url: '/rpc/Config.Save', 
-        type: 'POST', 
-        data: JSON.stringify({
-          "reboot": true
-        }),
-
-        /* On save */
-        success: function(data) {
-
-          /* Redirect */
-          window.location.href = "https://iowine-cloud.firebaseapp.com/";
-
-        }
-      });
-    },
+    success: saveConfig,
   })
-});
+
+};
+
+/**
+ * Save config values
+ */
+let saveConfig = function(_) {
+  $.ajax({
+    url: '/rpc/Config.Save', 
+    type: 'POST', 
+    data: JSON.stringify({
+      "reboot": true
+    }),
+    success: redirect
+  });
+};
+
+/**
+ * Redirect 
+ */
+let redirect = function(_) {
+  window.location.href = "https://iowine-cloud.firebaseapp.com/";
+}
